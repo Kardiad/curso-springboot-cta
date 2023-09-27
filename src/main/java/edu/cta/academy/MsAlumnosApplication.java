24781f9c73180860869fb8bba521e1a87b9dfc5d
kardiad <mrakami98@gmail.com>
2023-09-27 20:47:57 +0200

    arreglao

--- src/main/java/edu/cta/academy/MsAlumnosApplication.java
+++ src/main/java/edu/cta/academy/MsAlumnosApplication.java
@@ -3,7 +3,9 @@ package edu.cta.academy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MsAlumnosApplication extends SpringBootServletInitializer {
 
